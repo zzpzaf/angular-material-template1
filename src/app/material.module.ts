d079c65ef3228b9a275ecefba86f007ab2049f40
Panos Zafeiropoulos <zzpzaf@gmail.com>
2023-11-28 17:42:49 +0100

    1st Commit @angular/material has been added and set up with a different module: material.module.ts. Many (commonly used) material components have been added in it. MaterialModule has been imported in the app.module.ts (AppModule) – in imports section.

---
+++ src/app/material.module.ts
@@ -0,0 +1,79 @@
+import { NgModule } from  '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatListModule} from '@angular/material/list';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import {MatSelectModule} from '@angular/material/select';
+
+@NgModule({
+    imports: [MatButtonModule,
+              MatToolbarModule,
+              MatNativeDateModule,
+              MatDatepickerModule,
+              MatIconModule,
+              MatCheckboxModule, 
+              MatCardModule,
+              MatFormFieldModule,
+              MatInputModule,
+              MatRadioModule,
+              MatListModule,
+              MatMenuModule,
+              MatProgressSpinnerModule,
+              MatSidenavModule,
+              MatExpansionModule,
+              MatListModule,
+              MatCardModule,
+              MatMenuModule,
+              MatIconModule,
+              MatButtonModule,
+              MatTableModule,
+              MatSortModule,
+              MatSelectModule,
+
+
+             ],
+    exports: [MatButtonModule,
+              MatToolbarModule,
+              MatNativeDateModule,
+              MatDatepickerModule,
+              MatIconModule,
+              MatCheckboxModule, 
+              MatCardModule,
+              MatFormFieldModule,
+              MatInputModule,
+              MatRadioModule,
+              MatListModule,
+              MatMenuModule,
+              MatProgressSpinnerModule,
+              MatSidenavModule,
+              MatExpansionModule,
+              MatListModule,
+              MatGridListModule,
+              MatCardModule,
+              MatMenuModule,
+              MatIconModule,
+              MatButtonModule,
+              MatTableModule,
+              MatSortModule,
+              MatSelectModule,
+              
+            ], 
+})
+
+export  class  MaterialModule { }
+
+
